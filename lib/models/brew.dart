1d054bcb366afe46eea34990a3fbeb32c0728729
1DS22CS223-SUPRIYAR <supriyar3march2004@gmail.com>
2024-12-05 05:04:47 +0530

    Authentication_v1

---
+++ lib/models/brew.dart
@@ -0,0 +1,8 @@
+class Brew {
+
+  final String? name;  // nullable type
+  final String? sugars;  // nullable type
+  final int? strength;  // nullable type
+
+  Brew({ this.name, this.sugars, this.strength });
+}
---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,108 @@
+import 'package:soulscript/services/auth.dart';
+import 'package:soulscript/shared/constants.dart';
+import 'package:soulscript/shared/loading.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+
+  final Function? toggleView;
+  const SignIn({this.toggleView, Key? key}) : super(key: key);
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+  String error = '';
+  bool loading = false;
+
+  // text field state
+  String email = '';
+  String password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return loading ? Loading() : Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.green[400],
+        elevation: 0.0,
+        title: Text('SoulScript'),
+        actions: <Widget>[
+          TextButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Register'),
+            onPressed: () =>  widget.toggleView?.call(),
+          ),
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0),
+              TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'email'),
+                validator:  (val) {
+                  if (val == null || val.length < 6) {
+                    return 'Enter a password 6+ chars long';
+                  }
+                  return null;
+                },
+                onChanged: (val) {
+                  setState(() => email = val);
+                },
+              ),
+              SizedBox(height: 20.0),
+              TextFormField(
+                obscureText: true,
+                decoration: textInputDecoration.copyWith(hintText: 'password'),
+                validator: (val) {
+                  if (val == null || val.length < 6) {
+                    return 'Enter a password 6+ chars long';
+                  }
+                  return null;
+                },
+                onChanged: (val) {
+                  setState(() => password = val);
+                },
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.pink[400], // Correct property for background color
+                ),
+                child: Text(
+                  'Sign In',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    setState(() => loading = true);
+                    dynamic result = await _auth.signInWithEmailAndPassword(email, password);
+                    if (result == null) {
+                      setState(() {
+                        loading = false;
+                        error = 'Could not sign in with those credentials';
+                      });
+                    }
+                  }
+                },
+              ),
+
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(color: Colors.green, fontSize: 14.0),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
